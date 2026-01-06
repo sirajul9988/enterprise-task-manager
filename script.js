a2b7d51d7eac59f5b3c29fc740711a2b3faad16a
sirajul9988 <161524666+sirajul9988@users.noreply.github.com>
2026-01-06 22:10:01 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const task = input.value.trim();
+
+  if (task === "") return;
+
+  const li = document.createElement("li");
+  li.innerText = task;
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
